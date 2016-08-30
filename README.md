c8e3e8db53f2d2f6d2e5c2685357c63c999ab482
Michael Brandenburg <mcbrande@gmail.com>
2016-08-29 22:09:42 -0700

    Dir Info

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # dirInfo
 CLI in Go for hashing of files in a directory.
+
+Will go through either CWD or specified directory and al sub-directories outputing the results to a json file.
+
+
+## Commands
+**--path *value*, -p *value*** Root directory for the process. Defaults to CWD if none specified.
+
+**--info, -i** Get the system info for the file.
+
+**--note *value*, -n *value*** Useful for putting a note (Example: comparing two servers, working and notWorking)
+
+**--output *value*, -o *value*** Output file(Example -o foo will create foo.json)
+
+**--talkative,-t** Verbose output
+
+**--help, -h** CLI help
+
+**--version, -v** Version info
+
+
+### Hashing Flags
+  **--sha1, -s** SHA-1 hashing of the file.
+
+  **--sha256, --s2** SHA-256 hasing of the file.
+
+  **--md5, -m** MD5 hashing of the file.
+
+
+## Example
+
+Running `go run *.go --s2 -s -m -note 'for readme.md'` in the project directory will get the following output.
+
+```
+File Read Completed in 55.741498ms
+Data written to: iMac-results.json
+```
+
+```json
+[
+   {
+      "directory":"~/git-repo/src/dirInfo/",
+      "filepath":"~/git-repo/src/dirInfo/LICENSE",
+      "name":"LICENSE",
+      "size":1076,
+      "mode":420,
+      "machineName":"iMac",
+      "md5":"e101e9a69504fcc422e363f49cb48827",
+      "sha1":"152e742349f2eef53bd53ef9ee7cb3c08572a29d",
+      "sha256":"8f1f67e58e674ca7b9beebb86736eb391d5dafb31e53231106c54e4e239ae105",
+      "lastModified":"2016-08-29T21:44:52-07:00",
+      "note":"for readme.md"
+   },
+   {
+      "directory":"~/git-repo/src/dirInfo/",
+      "filepath":"~/git-repo/src/dirInfo/README.md",
+      "name":"README.md",
+      "size":1658,
+      "mode":420,
+      "machineName":"iMac",
+      "md5":"c75d7ab4e02ae1936ea67248e8883496",
+      "sha1":"68f291662caa826fa2c61fac6ae114000deaddef",
+      "sha256":"42840517bce7bea4433583ea27e5ee323469535102a4ce760038fb101bf2dbdb",
+      "extension":".md",
+      "lastModified":"2016-08-29T22:00:22-07:00",
+      "note":"for readme.md"
+   },
+   {
+      "directory":"~/git-repo/src/dirInfo/",
+      "filepath":"~/git-repo/src/dirInfo/main.go",
+      "name":"main.go",
+      "size":5213,
+      "mode":420,
+      "machineName":"iMac",
+      "md5":"7001ab02606cb481c2bfb840abafdae3",
+      "sha1":"fb87ef900453755e5d7dc219e40b08e4f715c230",
+      "sha256":"b44ac4fa3d4176c61f3004c6ba3ddf547209dc2415c4af5c2eba1dd3f6c679a8",
+      "extension":".go",
+      "lastModified":"2016-08-29T21:53:05-07:00",
+      "note":"for readme.md"
+   }
+]
+```
---
+++ main.go
@@ -0,0 +1,227 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/urfave/cli"
+)
+
+type FileData struct {
+	Directory    string      `json:"directory"`
+	FilePath     string      `json:"filepath"`
+	Name         string      `json:"name"`
+	Size         int64       `json:"size"`
+	Mode         os.FileMode `json:"mode"`
+	MachineName  string      `json:"machineName"`
+	MD5Hash      string      `json:"md5,omitempty"`
+	SHA1Hash     string      `json:"sha1,omitempty"`
+	SHA256Hash   string      `json:"sha256,omitempty"`
+	Extension    string      `json:"extension,omitempty"`
+	LastModified time.Time   `json:"lastModified"`
+	SystemInfo   interface{} `json:"systemInfo,omitempty"`
+	Note         string      `json:"note,omitempty"`
+}
+
+var root string
+var output string
+var md5Enabled bool
+var verbose bool
+var sha1Enabled bool
+var sha256Enabled bool
+var machineName string
+var note string
+var systemInfo bool
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "Directory Info"
+	app.Usage = "Looks in specified directory or directory executed from to get all file info"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{Name: "path, p", Usage: "Directory to start searching", Destination: &root},
+		cli.BoolFlag{Name: "sha1, s", Usage: "Enable SHA1 hashing of files.", Destination: &sha1Enabled},
+		cli.BoolFlag{Name: "sha256, s2", Usage: "Enable SHA256 hashing of files.", Destination: &sha256Enabled},
+		cli.BoolFlag{Name: "md5, m", Usage: "Enable MD5 hashing of files.", Destination: &md5Enabled},
+		cli.BoolFlag{Name: "info,i", Usage: "Get system info data", Destination: &systemInfo},
+		cli.StringFlag{Name: "note, n", Usage: "Note that will be attached to the data. Example:  '-n working'", Destination: &note},
+		cli.BoolFlag{Name: "talkative, t", Usage: "Verbose output for each file", Destination: &verbose},
+		cli.StringFlag{Name: "output, o", Usage: "Filename of the output. Example: '-o something'", Destination: &output},
+	}
+	app.Action = func(c *cli.Context) error {
+		startTime := time.Now()
+		if root == "" {
+			wd, err := os.Getwd()
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			root = wd
+		}
+
+		mn, err := os.Hostname()
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		machineName = mn
+
+		if output == "" {
+			output = fmt.Sprintf("%s-%s", machineName, "results")
+		}
+
+		fileInfo, err := getFileInfo()
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		err = writeFile(&fileInfo)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		fmt.Println(fmt.Sprintf("File Read Completed in %s ", time.Since(startTime).String()))
+		fmt.Println(fmt.Sprintf("Data written to: %s.json", output))
+		return nil
+
+	}
+
+	app.Run(os.Args)
+}
+
+func getFileInfo() ([]FileData, error) {
+
+	directoryFileInfo := []FileData{}
+	fullPath, err := filepath.Abs(root)
+
+	if err != nil {
+		return directoryFileInfo, err
+	}
+	funkyWalk := func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() {
+			return nil
+		}
+
+		fd := FileData{FilePath: path, Name: f.Name(), Directory: strings.TrimRight(path, f.Name()), Mode: f.Mode(), Size: f.Size(), MachineName: machineName, Extension: filepath.Ext(f.Name()), LastModified: f.ModTime()}
+
+		if systemInfo {
+			info := f.Sys()
+			if info != nil {
+				fd.SystemInfo = info
+			}
+		}
+
+		if note != "" {
+			fd.Note = note
+		}
+
+		if sha1Enabled {
+			sha1Result, err := getSHA1Hash(path)
+			if err != nil {
+				return err
+			}
+			fd.SHA1Hash = hex.EncodeToString(sha1Result)
+		}
+
+		if sha256Enabled {
+			sha256Result, err := getSHA256Hash(path)
+			if err != nil {
+				return err
+			}
+			fd.SHA256Hash = hex.EncodeToString(sha256Result)
+		}
+
+		if md5Enabled {
+			md5Result, err := getMD5Hash(path)
+			if err != nil {
+				return err
+			}
+			fd.MD5Hash = hex.EncodeToString(md5Result)
+		}
+
+		directoryFileInfo = append(directoryFileInfo, fd)
+
+		if verbose {
+			fmt.Println(fd.Name, fd.SHA1Hash, fd.MD5Hash, fd.SHA256Hash)
+		}
+
+		return nil
+	}
+	err = filepath.Walk(fullPath, funkyWalk)
+	return directoryFileInfo, err
+}
+
+func getMD5Hash(filePath string) ([]byte, error) {
+	var results []byte
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return results, err
+	}
+	defer f.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return results, err
+	}
+
+	bytes := hash.Sum(results)
+	return bytes, nil
+}
+
+func getSHA1Hash(filePath string) ([]byte, error) {
+	var results []byte
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return results, err
+	}
+	defer f.Close()
+
+	hash := sha1.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return results, err
+	}
+
+	bytes := hash.Sum(results)
+	return bytes, nil
+}
+
+func getSHA256Hash(filePath string) ([]byte, error) {
+	var results []byte
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return results, err
+	}
+	defer f.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return results, err
+	}
+
+	bytes := hash.Sum(results)
+	return bytes, nil
+}
+
+func writeFile(directoryFileInfo *[]FileData) error {
+	data, err := json.Marshal(directoryFileInfo)
+	err = ioutil.WriteFile(fmt.Sprintf("%s.json", output), data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
